abf12f663fc7a1650243ba9ee14ba3508f984de1
laurenarthur <arthur_lauren@columbusstate.edu>
2021-02-09 17:26:55 +0000

    All working

---
+++ my-app/src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+ 
+  // header Component
+
+const Header= () => {
+  return(
+    <header className="Header"> 
+      <h1>   Harry Potter Quiz </h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ my-app/src/Components/data.js
@@ -0,0 +1,23 @@
+let quizdata=[
+    
+    {
+        id:1,
+        quiz: "When Harry Potter and the Sorcerer's Stone Published?",
+        FindAnswer:["June 26, 1997", "June 27, 1997", "June 28, 1997"],
+        rightAnswer:"June 26, 1997"
+    },
+    {
+        id:2,
+        quiz: "What house does Harry Potter not want to be sorted in?",
+        FindAnswer:["Ravenclaw", "Hufflepuff", "Slytherin", "Gryffindor"],
+        rightAnswer:"Slytherin"
+    },
+    {
+        id:3,
+        quiz: "What is the address of Mr. and Mrs. Dursley?",
+        FindAnswer:["5 Privet Drive", "4 Prime Drive", "5 Prime Drive", "4 Privet Drive"],
+        rightAnswer:"4 Privet Drive"
+    },
+]
+
+export default quizdata;
